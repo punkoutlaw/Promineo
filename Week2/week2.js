20bd526e720f1dca5e1dd0d0ebc169d47ae65b1d
David Feudale <dwfeudale@gmail.com>
2022-02-09 13:57:09 -0500

    updated week2 file

---
+++ Week2/week2.js
@@ -0,0 +1,86 @@
+// create a variable called speedLimit and another called mySpeed
+// set their values to numbers between 1 and 100
+let speedLimit = 50;
+let mySpeed = 65;
+
+// using a conditional, determine if mySpeed is greater than the speedLimit
+// if true, print "Slow Down! Mom is mad!" to the console
+// if mySpeed is equal to the speedLimit, print "Everyone is happy!" to the console
+// if mySpeed is less than the speedLimit, print "Speed up! Dad is mad!"
+if (mySpeed > speedLimit) {
+    // if true, print "Slow Down! Mom is mad!" to the console
+    console.log('Slow Down! Mom is mad!');
+} else if (mySpeed == speedLimit) {
+    // if mySpeed is equal to the speedLimit, print "Everyone is happy!" to the console
+    console.log('Everyone is happy!');
+} else {
+    // Note that you DO NOT need to set a conditional on the final "else" statement!
+    // if mySpeed is less than the speedLimit, print "Speed up! Dad is mad!"
+    console.log('Speed up! Dad is mad!');
+}
+
+// create two variables, one named alarmSet, the other openDoor
+// set them to a boolean value
+let alarmSet = true;
+let openDoor = false;
+
+// using a conditional, determine if alarm is set.
+// NOTE: the below can also be shortened to "if(alarmSet && openDoor)"!
+if(alarmSet == true && openDoor == true) {
+    // if alarm is set and door is set to open, print "Sound Alarm!" to the console
+    console.log('Sound Alarm!');
+} else { 
+    // otherwise, print "Everything is fine." to the console.
+    console.log('Everything is fine.');
+}
+
+
+
+
+// create two variables, username and password
+let username;
+let password;
+// create a conditional, if the username is "Tommy123" and the password is "12345"
+// or the username is "Timmy456" and the password is "6789", print "Admin Login Successful" to the console
+if((username === 'Tommy123' && password === '12345') || (username == 'Timmy456' && password == '6789')) {
+    console.log('Admin Login Successful');
+} else {
+    // otherwise, print "Admin Access Denied"
+    console.log('Admin Access Denied');
+}
+
+
+
+// write code that will set the value of studentClass based on studentGrade
+// studentGrade will be K-12
+// K-6 will be Elementary
+// 7-8 will be Middle
+// 9 will be Freshman, 10 Sophomore, 11 Junior, 12 Senior
+// Anything other than these values will return "Error" to the console
+
+
+
+
+
+// write a for loop that will iterate backwards from 10 to -10
+
+
+
+// write a do/while loop that prints 1 through 50
+
+
+// edit the previous do/while loop so that it prints the remainder when the loop number is divided by 4
+
+
+// Someone messed up the following for loop
+// fix the following infinite loop, uncomment to test
+/* 
+    for(let i = 11; i > 10; i++){
+        console.log(i);
+    } 
+*/
+
+
+
+
+
