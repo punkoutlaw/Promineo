21fe2bc88f1794fac598e2f699002957faac16e4
David Feudale <dwfeudale@gmail.com>
2022-02-15 17:38:00 -0500

    added to functions.js

--- Week3/functions.js
+++ Week3/functions.js
@@ -20,4 +20,36 @@ function myFunction() {
     }
 }
 
-myFunction(); // Use the to CALL/INVOKE your function!
+myFunction(); // Use the to CALL TO/INVOKE your function!
+
+// We can put anything inside our function - conditionials, loops, variables, etc. can all be used in a function.
+// To define functions that take input, we add parameters inside the parenthesis of the function declaration.
+
+function createFullName(firstName, lastName) { // "firstName" is our parameter!
+    console.log(firstName + ' ' + lastName);
+}
+
+createFullName('Tom', 'Sawyer'); // " 'Tom', 'Sawyer' " is our arguement!
+
+// Parameters are like parking spots & arguements are like cars! The cars go into the parking spots!!
+// Remember that Parameters are only placeholders! Our argument will dictate what will apply to the function.
+
+// to add a return statement to return the full name:
+
+function createFullName(firstName, lastName) {
+    return (firstName + ' ' + lastName);
+}
+
+console.log(createFullName('Tom', 'Sawyer')); 
+
+// OR
+
+function createFullName(firstName, lastName) {
+    return (firstName + ' ' + lastName);
+}
+
+var fullName = createFullName('Tom', 'Sawyer');
+console.log('Welcome, ' + fullName);
+
+
+// Give functions descriptive names, typically verbs, since they are actions!
