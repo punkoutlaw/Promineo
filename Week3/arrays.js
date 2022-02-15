2b31a8c2f73dab0933976b466c03c8db8ab54ac6
David Feudale <dwfeudale@gmail.com>
2022-02-15 17:04:55 -0500

    added functions.js

---
+++ Week3/arrays.js
@@ -0,0 +1,31 @@
+// Arrays are a data type that allow us to group similar variables together and refer to the values by location.
+// The location of index iz zero based. Which means that the 1st value is located at posisiton 0 and the last will always be located at the index just one less than the length of the array, or the number of elements in the array.
+
+// Example:
+
+// a // position 0
+// b // position 1
+// c // position 2
+// d // position 3
+// e // position 4
+// f // position 5, which is one less than the length of the array
+
+var customerNames = [];
+customerNames.push('Sam Smith'); // position 0
+customerNames.push('Tommy Guns'); // position 1
+
+console.log(customerNames[0]);
+console.log(customerNames[1]);
+
+// The above can also be written as a for statement to make the code more dynamic and reduce lines:
+
+for (let i = 0; i < customerNames.length; i++) {
+    console.log(customerNames[i]);
+}
+
+// You can use a "for of" loop to shorten the code even further and get rid of the 'i" all-together:
+
+for (name of customerNames) {
+    console.log(name);
+}
+
---
+++ functions.js
@@ -0,0 +1,24 @@
+// To create a function, we need 3 things:
+// 1) The "function" keyword
+// 2) The "name" (variable) we wish to give the function
+// 3) A set of curly braces {}
+
+// Example:
+
+// function myFunction() {
+//     for (let i = 0; i < 100; i++) {
+//         console.log(i);
+//     }
+// }
+
+// NOTE: The above code WILL NOT execute because we have not referred anything to the function name.
+// Nothing happens until we call the function we wrote. To invoke the function, call the function by its name:
+
+function myFunction() {
+    for (let i = 0; i < 100; i++) {
+        console.log(i);
+    }
+}
+
+myFunction() // Use the to CALL/INVOKE your function!
+
