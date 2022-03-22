6b632f453194013a7009b9884823088508ea916d
David Feudale <dwfeudale@gmail.com>
2022-03-22 16:28:09 -0400

    updated week6 files

---
+++ Week6/wcd-test.js
@@ -0,0 +1,11 @@
+
+var expect = chai.expect;
+
+describe('MyFunction', function() {
+    describe('#playTopCard', function() {
+        it('should play the top card', function() {
+            const newCard = new Card();
+            expect(newCard.value).to.equal();
+        });
+    });
+});
