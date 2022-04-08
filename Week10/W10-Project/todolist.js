36f1eabbd0665114bc6a055ccc525a35fd94cf4c
David Feudale <dwfeudale@gmail.com>
2022-04-07 23:03:31 -0400

    created todolist files

---
+++ Week10/W10-Project/todolist.js
@@ -0,0 +1,32 @@
+let id = 0;
+
+document.getElementById('add').addEventListener('click', () => {
+    let createdDate = new Date();
+    let table = document.getElementById('list');
+    let row = table.insertRow(1);
+    row.setAttribute('id', `item-${id}`);
+    row.insertCell(0).innerHTML = document.getElementById('new-task').value;
+    row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
+    row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
+    row.insertCell(2).innerHTML = document.getElementById('new-end-date').value;
+    let actions = row.insertCell(4);
+    actions.appendChild(createDeleteButton(id++));
+    document.getElementById('new-task').value = '';
+});
+
+
+// This function will create a button and bind it to a method or function to the buttons on-click properties.
+// When it's clicked it will delete the row that the button is assigned to.
+
+function createDeleteButton(id) { 
+    let btn = document.createElement('button'); 
+    btn.className = 'btn btn-primary'
+    btn.id = id;
+    btn.innerHTML = 'Delete';
+    btn.onclick = () => {
+        console.log(`Deleting row with id: item-${id}`);
+        let elementToDelete = document.getElementById(`item-${id}`);
+        elementToDelete.parentNode.removeChild(elementToDelete);
+    };
+    return btn;
+}
