4d84b006d5150b5bed8cf1d4511c8fc979ef4a67
David Feudale <dwfeudale@gmail.com>
2022-04-07 23:29:16 -0400

    added teamapp files/removed node_modules

---
+++ Week10/W10-Project/teamapp.js
@@ -0,0 +1,7 @@
+/* We will be creating a team app where each team will have a table 
+and each table will have the ability to create, update and delete player information 
+
+We will be able to add new teams which will create new html tables 
+and each table will manipulate and manage data for that team.
+
+*/
