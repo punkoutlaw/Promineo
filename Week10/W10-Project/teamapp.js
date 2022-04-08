60c16c67ddfc41f88e2a902e465e5afe396cb71a
David Feudale <dwfeudale@gmail.com>
2022-04-07 23:46:32 -0400

    added teamapp files

--- Week10/W10-Project/teamapp.js
+++ Week10/W10-Project/teamapp.js
@@ -5,3 +5,27 @@ We will be able to add new teams which will create new html tables
 and each table will manipulate and manage data for that team.
 
 */
+
+class Member {
+    constructor(name, position) {
+        this.name = name;
+        this.position = position
+    }
+}
+
+class Team {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.members = []; // This wont be passed in but it will start as an empty array which will be all the members added to the team.
+    }
+
+    addMember(member) {
+        this.members.push(member) // This will add memebers to the team.
+    }
+
+    deleteMember(member) {
+        let index = this.members.indexOf(member);
+        this.members.splice(index, 1); // This will delete members from the team.
+    }
+}
