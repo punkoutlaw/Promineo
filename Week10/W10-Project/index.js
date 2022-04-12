06a6747803bc52f06cb39a1dcb4d1ca7a984deb7
David Feudale <dwfeudale@gmail.com>
2022-04-11 20:26:35 -0400

    updated files

--- Week10/W10-Project/index.js
+++ Week10/W10-Project/index.js
@@ -159,7 +159,7 @@ document.getElementById('add').addEventListener('click', () => {
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
     row.insertCell(0).innerHTML = document.getElementById('new-task').value;
-    row.insertCell(1).innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
+    row.insertCell(1).innerHTML = `${createdDate.getHours()}-${createdDate.getMinutes()}-${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
     row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
     row.insertCell(2).innerHTML = document.getElementById('new-end-date').value;
     let actions = row.insertCell(4);
