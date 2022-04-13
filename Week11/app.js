96665060beb106d3f76e5ce20a6ea42480a4c251
David Feudale <dwfeudale@gmail.com>
2022-04-13 14:29:00 -0400

    updated week11 files

--- Week11/app.js
+++ Week11/app.js
@@ -1,27 +1,44 @@
+// VIDEO 1 \\
+
 // console.log($('body'));
 
-let p = $('#test');
-let div = $('.my-class');
-let ul = $('ul');
+// VIDEO 2 \\
 
-console.log(p);
-console.log(div);
-console.log(ul);
+// let p = $('#test');
+// let div = $('.my-class');
+// let ul = $('ul');
 
-console.log(p.text());
-p.text('New Text')
+// console.log(p);
+// console.log(div);
+// console.log(ul);
+
+// console.log(p.text());
+// p.text('New Text')
 
 // $('input').val('New Value');
 
-$('input').attr("placeholder", 'Placeholder Text');
+// $('input').attr("placeholder", 'Placeholder Text');
 
-div.prepend('<p>prepended paragraph</p>');
-div.append('<p>appended paragraph</p>');
-div.before('<p>paragraph that was added before the div</p>')
-div.after('<p>paragraph added after the div</p>');
+// div.prepend('<p>prepended paragraph</p>');
+// div.append('<p>appended paragraph</p>');
+// div.before('<p>paragraph that was added before the div</p>')
+// div.after('<p>paragraph added after the div</p>');
 
-div.empty();
-ul.remove();
+// div.empty();
+// ul.remove();
 
-$('input').hide();
-setTimeout(() => $('input').show(), 2000);
+// $('input').hide();
+// setTimeout(() => $('input').show(), 2000);
+
+// VIDEO 3 \\
+
+// GET METHOD:
+
+$.get('https://reqres.in/api/users/2', (data) => console.log(data));
+
+// POST METHOD:
+
+$.post('https://reqres.in/api/users', {
+    name: 'Tommy',
+    job: 'Front End Software Developer'
+}, (data) => console.log(data));
