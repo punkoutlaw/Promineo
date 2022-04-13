f7ffcbcb34bfe37068339f96441b2a4b4712feb1
David Feudale <dwfeudale@gmail.com>
2022-04-13 14:18:47 -0400

    updated week11 files

--- Week11/app.js
+++ Week11/app.js
@@ -1,1 +1,27 @@
-console.log($('body'));
+// console.log($('body'));
+
+let p = $('#test');
+let div = $('.my-class');
+let ul = $('ul');
+
+console.log(p);
+console.log(div);
+console.log(ul);
+
+console.log(p.text());
+p.text('New Text')
+
+// $('input').val('New Value');
+
+$('input').attr("placeholder", 'Placeholder Text');
+
+div.prepend('<p>prepended paragraph</p>');
+div.append('<p>appended paragraph</p>');
+div.before('<p>paragraph that was added before the div</p>')
+div.after('<p>paragraph added after the div</p>');
+
+div.empty();
+ul.remove();
+
+$('input').hide();
+setTimeout(() => $('input').show(), 2000);
