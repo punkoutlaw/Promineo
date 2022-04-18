3c0ce6dc663a342495cf4b7ad3fff10faeb2b737
David Feudale <dwfeudale@gmail.com>
2022-04-18 16:40:28 -0400

    updated guessing game

--- Week11/Class/guessing-game.js
+++ Week11/Class/guessing-game.js
@@ -1,16 +1,40 @@
 
 // Choose a random value between 1 and 100:
 
-let randomValue = Math.floor(Math.random() * 100) - 1
+let randomValue = Math.floor(Math.random() * 100) - 1 // The Math.floor() method rounds a number downwards to the nearest integer, and returns the result.
 let userGuess = -1;
 
 console.log(randomValue);
 
-$('text').on('blur', function() {
+$('#text').on('blur', function() {
     userGuess = $('#text').val();
-    console.log(userGuess)
+    console.log(userGuess);
+    checkGuess();
 });
 
 function checkGuess(){
-    if(userGuess > randomValue) 
-} console.log('user is too high');
+    if(userGuess > randomValue) {
+        console.log('user is too high');
+        $('.error-list').append(`<p>User Guess of ${userGuess} is too high</p>`);
+        $('.error-list').css('color', '#9AC305');
+
+    } else if(userGuess < randomValue)
+
+    {
+        console.log('user is too low');
+        $('.error-list').append(`<p>User Guess of ${userGuess} is too low</p>`);
+        $('.error-list').css('color', '#9AC305');
+        
+    } else {
+        
+        // When the user wins, the background of the page will change to green
+        console.log('user wins');
+        $('.error-list').remove();
+        $('input').remove();
+        $('label').remove();
+        $('#bg').css('background', 'green');
+        $('.container').append(`<p>Congrats, you have won!</p>`);
+        $('.container p').css('color', '#9AC305');
+    }
+}
+
