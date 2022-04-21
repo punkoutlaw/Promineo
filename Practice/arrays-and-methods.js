b35e9977a9d3046b1bc171666e171f59daee211c
David Feudale <dwfeudale@gmail.com>
2022-04-21 15:18:54 -0400

    removed node_mules from W12 & updated practice files

--- Practice/arrays-and-methods.js
+++ Practice/arrays-and-methods.js
@@ -1,12 +1,25 @@
-// PRACTICE \\
+// PRACTICE ARRAYS & METHODS \\
+
+// To add the following array, we can use several different ways/methods.
 
 var arr = [1,-4,7,12];
 
-// let sum = 0;
-// for(let a of arr)
-//     sum += a;
-// console.log(sum);
+// METHOD 1 - USING A VARIABLE AND FOR LOOP \\
 
-let sum = arr.reduce(
+let sum1 = 0;
+for(let a of arr)
+    sum1 += a;
+console.log(sum1);
+
+// METHOD 2 - USING THE REDUCE METHOD \\
+
+let sum2 = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0); console.log(sum2)
+
+// METHOD 3 - SIMPLIFIED REDUCE METHOD \\
+
+let sum3 = arr.reduce(
     (a, c) => a + c); 
-console.log(sum);
+console.log(sum2);
+
