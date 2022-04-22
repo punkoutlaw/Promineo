496dc3f7d06fdccd08f9bbe8df6419c4adb860fd
David Feudale <dwfeudale@gmail.com>
2022-04-22 16:50:49 -0400

    updated W12 project files

--- Week12/W12-Project/index.js
+++ Week12/W12-Project/index.js
@@ -6,18 +6,74 @@
 
 */
 
-class Cars {
-    make
-    model
-    color
-    year
-    bodyType
+// class Cars {
+//     make
+//     model
+//     color
+//     year
+//     bodyType
 
-    constructor(make, model, color, year, bodyType, mileage) {
-        this.make = make;
-        this.model = model;
-        this.color = color;
-        this.year = year;
-        this.bodyType = bodyType
-    }
+//     constructor(make, model, color, year, bodyType, mileage) {
+//         this.make = make;
+//         this.model = model;
+//         this.color = color;
+//         this.year = year;
+//         this.bodyType = bodyType
+//     }
+// }
+
+var app = new function() {
+    this.el= document.getElementById('tasks');
+    this.tasks=[]
+
+    // CREATE \\
+    this.Add = function() {
+        el = document.getElementById('add-to-do');
+        var tasks = el.value;
+        if(tasks) {
+            this.tasks.push(tasks.trim());
+            this.el.value ='';
+            this.FetchAll();
+        }
+
+    };
+
+    // READ \\
+    this.FetchAll = function() {
+        var data ='';
+
+        if(this.tasks.length > 0) {
+            for(i = 0; i < this.tasks.length; i++) {
+                data += '<tr>';
+                data += '<td>' + (i + 1) + ". " + this.tasks[i] + '</td>';
+                data += '<td><button onclick="app.Edit('+i+')" class="btn btn-warning">Edit</button></td>';
+                data += '<td><button onclick="app.Edit('+i+')" class="btn btn-danger">Delete</button></td>';
+                data += '</tr>'   
+            }
+        }
+        this.Count(this.tasks.length);
+        return this.el.innerHTML = data;
+
+    };
+
+    // UPDATE \\
+    this.Edit = function(item) {
+
+    };
+
+    // DELETE \\
+    this.Delete = function(item) {
+        
+    };
+
+    this.Count = function(data) {
+
+    };
+
+}
+
+app.FetchAll();
+
+function CloseInput() {
+    document.getElementById('edit-box').style.display = 'none';
 }
