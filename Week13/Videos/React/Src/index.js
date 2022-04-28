31f31ffbba8419a518712b14a3d142ff51f5319c
David Feudale <dwfeudale@gmail.com>
2022-04-28 13:48:25 -0400

    added more files in Video dir

--- Week13/Videos/React/Src/index.js
+++ Week13/Videos/React/Src/index.js
@@ -1,9 +1,11 @@
 /* 
-1) To install react in the directory, run "npm install react --save-dev" in cmd prompt.
+
+** HOW TO CREATE BOILER-PLATE CODE FOR REACT**
+
+1) First, install react in the directory, run "npm install react --save-dev" in cmd prompt.
 2) Next, run "npm install react-dom --save-dev" in the same directory.
 3) Next, run "npm install webpack --save-dev".
 4) Next, run "npm install webpack-cli --save-dev.
-5) Next, to set up a react template, type "rafce" inside the JS file and press enter.
 */
 
 import React from 'react';
