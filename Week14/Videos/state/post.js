2e0911bbf8220a6c6137e2b4d5b077714947e7e6
David Feudale <dwfeudale@gmail.com>
2022-05-05 14:44:49 -0400

    Updated W14 files

--- Week14/Videos/state/post.js
+++ Week14/Videos/state/post.js
@@ -10,6 +10,24 @@ import Comment from './comment';
 Please note - you HAVE to include "props" in your constructor, then you can use it inside each component.
 You always need to call the "super" constructor on the 1st line, then pass the props in.
 
+// UPDATING THE STATE \\
+
+*setTimeout needs to be done within the render, because every time you set the state it will re-render the component and it will create an infinite loop*
+The setState takes an object as its 1st parameter (content: 'state update') for example.
+
+It can also take a second parameter, which is called a "callback" which is followed by the 1st parameter using a comma to seperate them.
+Example: , () => setTimeout(() => this.setState({comments: tempComments}), 2000)). notice this is directly followed by the 1st parameter, but before the timeout.
+
+Note: You must NOT use "this.sate" or "this.props" directly in the updated values of the state.
+Example: this.setState({count: this.state.count + 1}) - it will update both values asynchronously and you cannot rely on the values.
+
+Instead, use the overriding of this.setState which takes the previous styate as an argument.
+Example: this.setState(state => ({count: state.count + 1})) which will return the object {}.
+
+Furthermore, you can add 2nd argument that would update your props.
+
+Example: setTimeout(() => this.setState((state, props) => ({count: state.count + 1})) which will return the object {}.
+
 */
 
 export default class Post extends React.Component {
@@ -17,11 +35,14 @@ export default class Post extends React.Component {
         super(props);
         this.state = {
             comments: props.comments,
-            content: props.content
+            content: props.content,
+            count: 0
         };
     }
     render() {
-        this.setState({content: 'state updated'})
+        let tempComments = this.state.comments;
+        setTimeout(() => this.setState({content: 'state updated', comments: []}, 
+        () => setTimeout(() => this.setState({comments: tempComments}), 2000)), 2000);
         let comments = [];
         if(this.state.comments) {
             for(let comment of this.state.comments) {
