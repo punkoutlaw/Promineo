674f4e99c47ebc2e5979fbd43dbc08e26ed5c904
David Feudale <dwfeudale@gmail.com>
2022-05-07 16:08:49 -0400

    updated data

--- Week14/W14-Project/src/components/reviews.js
+++ Week14/W14-Project/src/components/reviews.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Stars from "./stars";
 
 const Reviews = () => {
     const [review, setReview] = useState(null);
   return (
     <div>
-    <form className="form-control">
-    <Stars />
-        <span className="formtext"></span>
-            <input className="form-control" type="text" placeholder="Write a review" required />
-            <br></br>
-            <button className="btn btn-primary" type="submit" placeholder="Submit Review">Submit</button>
-    </form>
+        <form className="form-control">
+            <Stars />
+            <span className="formtext"></span>
+                <input 
+                className="form-control" 
+                type="text" name="review" 
+                placeholder="Write a review" 
+                value={review}
+                required />
+
+                <br></br>
+
+                <button 
+                className="btn btn-primary" 
+                type="submit" 
+                placeholder="Submit Review"
+                onClick={() => setReview(review)}
+                >Submit</button>
+        </form>
 </div>
   )
 }
