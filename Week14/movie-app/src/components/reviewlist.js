db374b8f29c7d2b33207197e96ce0f8ab26e4405
David Feudale <dwfeudale@gmail.com>
2022-05-10 02:04:56 -0400

    added files

--- Week14/movie-app/src/components/reviewlist.js
+++ Week14/movie-app/src/components/reviewlist.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Review from './review';
 
 class ReviewList extends React.Component {
     render() {
         return(
             <div className="container">
-
+                <Review />
             </div>
         );
     }
