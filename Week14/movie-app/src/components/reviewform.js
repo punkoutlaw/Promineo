feefda36628e93501eea7adbcf6a33cc90f03286
David Feudale <dwfeudale@gmail.com>
2022-05-09 22:27:49 -0400

    added components

---
+++ Week14/movie-app/src/components/reviewform.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ReviewForm extends React.Component {
+    render() {
+        return(
+
+        );
+    }
+}
+
+export default ReviewForm
---
+++ Week14/movie-app/src/components/reviewlist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ReviewList extends React.Component {
+    render() {
+        return(
+
+        );
+    }
+}
+
+export default ReviewList
