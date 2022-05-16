83deab285e0bd62a431f9d23921a95f18f4ad371
David Feudale <dwfeudale@gmail.com>
2022-05-16 16:49:24 -0400

    updated/added W15 files

--- Week15/Videos/http_rqst-fetch.js
+++ Week15/Videos/http_rqst-fetch.js
@@ -1,1 +1,37 @@
 const ENDPOINT = "https://ancient-taiga-31359.herokuapp.com/api/houses";
+
+fetch(ENDPOINT)
+.then(response => response.json())
+.then(response => {
+	console.log(response[0].rooms[0].name);
+});
+
+/* fetch(ENDPOINT)
+.then(response => response.json())
+.then(response => {
+  response.forEach(house => document.write(house.name + '<br>'));
+}); */
+
+/* fetch(ENDPOINT, {
+  method: 'POST',  // This is the rqst details
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    name: 'My Fetch House'
+  })
+}); */
+
+/* fetch(`${ENDPOINT}/62828c35e5b1170017ce4443`,  {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    name: 'My Fetch House',
+    rooms: [
+      {name: 'Front Room', area: 200},
+      {name: 'Kitchen', area: 220}
+    ]
+  })
+}); */
