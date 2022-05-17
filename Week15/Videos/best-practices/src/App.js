dfc686b3ae7b3fb36f934a0ef8e6445647432c60
David Feudale <dwfeudale@gmail.com>
2022-05-17 15:23:58 -0400

    added best-practices app & files

---
+++ Week15/Videos/best-practices/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalComponents from './components/functional-components';
+import Hooks from './components/hooks';
+
+function App() {
+  return (
+    <div>
+      <FunctionalComponents />
+      <Hooks />
+    </div>
+  );
+}
+
+export default App;
