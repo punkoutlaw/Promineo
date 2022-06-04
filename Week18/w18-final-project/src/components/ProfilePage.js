cfaf066c14d6a1ac87986480ee90de295ace5530
David Feudale <dwfeudale@gmail.com>
2022-06-04 12:58:12 -0400

    updated final project files

--- Week18/w18-final-project/src/components/ProfilePage.js
+++ Week18/w18-final-project/src/components/ProfilePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navigation from './Navigation';
-import { Card } from 'react-bootstrap';
+import { Card, Container, Col, Row } from 'react-bootstrap';
+import ProfileIcon from './ProfileIcon';
+import image from '../images/Dave_headshot.jpg';
 
 const ProfilePage = () => {
   return (
@@ -8,10 +10,28 @@ const ProfilePage = () => {
         <Navigation />
         <main style={{ padding: "2rem 0" }}>
         <Card>
-            <Card.Header><h2>Profile Page</h2></Card.Header>   
+            <Card.Header>
+                <div className='menu'>
+                <span><strong>Profile Page</strong>  &nbsp; </span><ProfileIcon className='icon' iconSize='medium' image={image}/>
+                </div>
+                </Card.Header>
             <Card.Body>
-                <Card.Title> Hello </Card.Title>
-                <Card.Text> Let's do this. </Card.Text>
+                <Card.Title></Card.Title>
+                <Container fluid>
+                <Card.Text className='profileBody'>
+                    <Row md={12}>
+                            <Col xs={4}><img src='https://picsum.photos/100/250' className='profileImg'/></Col>
+                            <Col xs={4}><img src='https://picsum.photos/200/250' className='profileImg'/></Col>
+                            <Col xs={4}><img src='https://picsum.photos/300/250' className='profileImg'/></Col>
+                        </Row>
+                        <br></br>
+                        <Row md={12}>
+                            <Col xs={4}><img src='https://picsum.photos/400/250' className='profileImg'/></Col>
+                            <Col xs={4}><img src='https://picsum.photos/500/250' className='profileImg'/></Col>
+                            <Col xs={4}><img src='https://picsum.photos/600/250' className='profileImg'/></Col>
+                        </Row>
+                </Card.Text>
+                </Container>
             </Card.Body>
             <Card.Footer> © 2022 INSTAGRAM FROM META </Card.Footer>
         </Card>
